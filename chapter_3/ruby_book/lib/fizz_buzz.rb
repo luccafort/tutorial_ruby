0be5d32354532f8294defbb67a2a834ef5a8000a
luccafort <muspellsheim1985@gmail.com>
2017-12-24 15:50:32 +0900

    最終的なテストコードの形に実装
    愚直なテストコードを実装

--- chapter_3/ruby_book/lib/fizz_buzz.rb
+++ chapter_3/ruby_book/lib/fizz_buzz.rb
@@ -17,5 +17,13 @@ class FizzBuzzTest < Minitest::Test
         assert_equal '1', fizz_buzz(1)
         assert_equal '2', fizz_buzz(2)
         assert_equal 'Fizz', fizz_buzz(3)
+        assert_equal '4', fizz_buzz(4)
+        assert_equal 'Buzz', fizz_buzz(5)
+        assert_equal 'Fizz', fizz_buzz(6)
+        assert_equal '7', fizz_buzz(7)
+        assert_equal 'Fizz Buzz', fizz_buzz(15)
+        assert_equal '16', fizz_buzz(16)
+        # Failureになるテストできちんと失敗を検知できるかチェック
+        #assert_equal 'Fizz Buzz', fizz_buzz(16)
     end
 end
