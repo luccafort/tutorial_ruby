3c1161af31860e3f22bcaa6e5bc89a02476e0f76
luccafort <muspellsheim1985@gmail.com>
2017-12-24 15:40:59 +0900

    サンプルのテストコードにFailureとErrorになるコードを追加
    コメントに状態を追加

--- chapter_3/sample_test.rb
+++ chapter_3/sample_test.rb
@@ -2,6 +2,11 @@ require 'minitest/autorun'
 
 class SampleTest < Minitest::Test
     def test_sample
+        # 成功する
         assert_equal 'RUBY', 'ruby'.upcase
+        # 1文字目のみ大文字にしているのでFailになる
+        #assert_equal 'RUBY', 'ruby'.capitalize
+        # nilからupcaseは呼び出せないのでErrorになる
+        #assert_equal 'RUBY', nil.upcase
     end
 end
