8dca54fc165e5070dfa83b8e261d9e7d1d0824af
luccafort <muspellsheim1985@gmail.com>
2018-01-07 13:56:38 +0900

    テストケース1を実装

--- chapter_7/ruby_book/test/gate_test.rb
+++ chapter_7/ruby_book/test/gate_test.rb
@@ -3,6 +3,13 @@ require_relative '../lib/gate'
 
 class GateTest < Minitest::Test
     def test_gate
-        assert Gate.new
+        # 150円の切符購入
+        # 梅田で入場、十三で出場
+        # 期待値: 出場できる
+        umeda = Gate.new(:umeda)
+        juso = Gate.new(:juso)
+        ticket = Ticket.new(150)
+        umeda.enter(ticket)
+        assert juso.exit(ticket)
     end
 end
