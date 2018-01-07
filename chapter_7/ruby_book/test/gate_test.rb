79af1f684a2e3e5d7c9eed211f0983504c93fd5a
luccafort <muspellsheim1985@gmail.com>
2018-01-07 14:27:08 +0900

    リファクタリング
    GateTestクラスにsetupメソッドを追加し各駅をインスタンス変数として定義するよう変更
    各テストケースで駅を参照する際の変数をインスタンス変数に変更

--- chapter_7/ruby_book/test/gate_test.rb
+++ chapter_7/ruby_book/test/gate_test.rb
@@ -3,26 +3,27 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
+    def setup
+        @umeda = Gate.new(:umeda)
+        @juso = Gate.new(:juso)
+        @mikuni = Gate.new(:mikuni)
+    end
+
     def test_umeda_to_juso
         # 150円の切符購入
         # 梅田で入場、十三で出場
         # 期待値: 出場できる
-        umeda = Gate.new(:umeda)
-        juso = Gate.new(:juso)
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
-        assert juso.exit(ticket)
+        @umeda.enter(ticket)
+        assert @juso.exit(ticket)
     end
 
     def test_umeda_to_mikuni_when_fare_is_not_enough
         # 150円の切符購入(本来の運賃は190円)
         # 梅田で入場、三国で出場
         # 期待値: 出場できない(運賃不足)
-        umeda = Gate.new(:umeda)
-        mikuni = Gate.new(:mikuni)
-        
         ticket = Ticket.new(150)
-        umeda.enter(ticket)
-        refute mikuni.exit(ticket)
+        @umeda.enter(ticket)
+        refute @mikuni.exit(ticket)
     end
 end
