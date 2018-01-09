9c8ef338b6c699c79a505e88f5b6bc9a7a570630
luccafort <muspellsheim1985@gmail.com>
2018-01-10 07:58:55 +0900

    chapter_8のファイルを追加
    モジュールの仮実装の追加
    テストコードの仮実装を追加

---
+++ chapter_8/ruby_book/lib/deep_freezable.rb
@@ -0,0 +1,5 @@
+module DeepFreezable
+    def deep_freezable
+        # TODO:あとで実装
+    end
+end
---
+++ chapter_8/ruby_book/test/deep_freezable_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require_relative '../lib/deep_freezable'
+
+class DeepFreezableTest
+    def test_deep_freeze
+        # TODO: あとで実装
+        assert DeepFreezable
+    end
+end
