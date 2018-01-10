cd23e594f27e703d98b758ae0511ad26c2086089
luccafort <muspellsheim1985@gmail.com>
2018-01-10 21:48:48 +0900

    ハッシュのテストコードを仮実装
    hash値に対してfreezeされているかのテストコードを仮実装

--- chapter_8/ruby_book/test/deep_freezable_test.rb
+++ chapter_8/ruby_book/test/deep_freezable_test.rb
@@ -3,12 +3,22 @@ require_relative '../lib/bank'
 require_relative '../lib/team'
 
 class DeepFreezableTest < Minitest::Test
-    def test_deep_freeze
+    def test_deep_freeze_array
         # 配列の値が正しいか?
         assert_equal ['Japan', 'US', 'India'], Team::COUNTRIES
         # 配列の値がfreezeされているか?
         assert Team::COUNTRIES.frozen?
         # 配列の要素全てがfreezeされｒているか
         assert Team::COUNTRIES.all? { |country| country.frozen? }
+    end
+
+    def test_deep_freeze_hash
+        # ハッシュの値が正しいか
+        assert_equal({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'}, Bank::CURRENCIES)
+
+        # ハッシュ自身がfreezeされているか
+        Bank::CURRENCIES.frozen?
+        # ハッシュの要素がfreezeされているか
+        assert Bank::CURRENCIES.all? { |current| current.frozen? }
     end
 end
