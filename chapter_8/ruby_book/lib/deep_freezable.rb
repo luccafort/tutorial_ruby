21fb7c6df22dc7c1ccdf54054614b28bfd620ad9
luccafort <muspellsheim1985@gmail.com>
2018-01-10 21:42:58 +0900

    deep_freeze()の実装を追加
    配列の各要素とその配列自身をfreezeされる実装を追加

--- chapter_8/ruby_book/lib/deep_freezable.rb
+++ chapter_8/ruby_book/lib/deep_freezable.rb
@@ -1,5 +1,11 @@
 module DeepFreezable
     def deep_freeze(array_or_hash)
-        # TODO:あとで実装
+        # 配列の各要素をfreeze
+        array_or_hash.each do |element|
+            element.freeze
+        end
+
+        # 配列自身をfreeze
+        array_or_hash.freeze
     end
 end
