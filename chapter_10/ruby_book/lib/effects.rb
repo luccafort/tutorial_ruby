57655fab034c3534c148593e611b44cfac29d685
luccafort <muspellsheim1985@gmail.com>
2018-01-13 15:35:21 +0900

    エコーエフェクトを追加
    スペース以外の入力された文字を繰り返す実装を追加
    複数回繰り返す実装のため2回と3回で増え方が変わっているかテストするコードを追加

--- chapter_10/ruby_book/lib/effects.rb
+++ chapter_10/ruby_book/lib/effects.rb
@@ -1,7 +1,15 @@
 module Effects
+    # 逆順
     def self.reverse
         ->(words) do
             words.split(' ').map(&:reverse).join(' ')
+        end
+    end
+
+    # 繰り返し
+    def self.echo(rate)
+        ->(words) do
+            words.chars.map {|c| c == ' ' ? c : c * rate}.join
         end
     end
 end
--- chapter_10/ruby_book/test/effects_test.rb
+++ chapter_10/ruby_book/test/effects_test.rb
@@ -2,8 +2,18 @@ require 'minitest/autorun'
 require './lib/effects'
 
 class EffectsTest < Minitest::Test
+    ORIGINAL_WORD = 'Ruby is fun!'.freeze
+
     def test_reverse
        effect = Effects.reverse
-       assert 'ybuR si !nuf', effect.call('Ruby is fun!')
+       assert 'ybuR si !nuf', effect.call(ORIGINAL_WORD)
+    end
+
+    def test_echo
+        effect = Effects.echo(2)
+        assert 'RRuubbyy iiss ffuunn!!', effect.call(ORIGINAL_WORD)
+
+        effect = Effects.echo(3)
+        assert 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call(ORIGINAL_WORD)
     end
 end
