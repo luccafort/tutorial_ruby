1c774ff2a75f6d05fbcb0956ab291ab5c0a270f8
luccafort <muspellsheim1985@gmail.com>
2018-01-13 15:44:00 +0900

    ラウドエフェクトの実装
    入力された文字列を大文字化し末尾に指定回数分の"!"を付与する実装を追加
    付与する"!"の回数が異なるテストコードを実装
    assertではなくassert_equalで同値で比較できていなかったため修正

--- chapter_10/ruby_book/test/effects_test.rb
+++ chapter_10/ruby_book/test/effects_test.rb
@@ -6,14 +6,22 @@ class EffectsTest < Minitest::Test
 
     def test_reverse
        effect = Effects.reverse
-       assert 'ybuR si !nuf', effect.call(ORIGINAL_WORD)
+       assert_equal 'ybuR si !nuf', effect.call(ORIGINAL_WORD)
     end
 
     def test_echo
         effect = Effects.echo(2)
-        assert 'RRuubbyy iiss ffuunn!!', effect.call(ORIGINAL_WORD)
+        assert_equal 'RRuubbyy iiss ffuunn!!', effect.call(ORIGINAL_WORD)
 
         effect = Effects.echo(3)
-        assert 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call(ORIGINAL_WORD)
+        assert_equal 'RRRuuubbbyyy iiisss fffuuunnn!!!', effect.call(ORIGINAL_WORD)
+    end
+
+    def test_loud
+        effect = Effects.loud(2)
+        assert_equal 'RUBY!! IS!! FUN!!!', effect.call(ORIGINAL_WORD)
+
+        effect = Effects.loud(3)
+        assert_equal 'RUBY!!! IS!!! FUN!!!!', effect.call(ORIGINAL_WORD)
     end
 end
